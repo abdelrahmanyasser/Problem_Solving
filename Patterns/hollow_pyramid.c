748e783210cf314fdea17be6140a3d285290b4c8
Abdelrahman Yasser <arahman.ysr@gmail.com>
2022-10-11 22:53:50 +0200

    Dayx: Implemented a hollow pyramid draw pattern

---
+++ Patterns/hollow_pyramid.c
@@ -0,0 +1,39 @@
+/*
+ * @author: Abdelrahman Yasser
+ *  @breif: hollow pyramid pattern implementation
+ */
+
+#include <stdio.h>
+#define SIZE 10
+
+int main()
+{
+    int row, i;
+
+    /* Print rows in sequence */
+    for (row = 0; row < SIZE; row++)
+    {
+        /* Print the spaces first */
+        for (i = 0; i < SIZE - row; i++)
+        {
+            printf(" ");
+        }
+
+        /* print stars at the edge */
+        for (i = 0; i < (2*row) +1; i++)
+        {
+            if( row == SIZE -1 || i == 0 || i == (2*row) )
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        
+        /* New line */
+        puts("");  
+    }
+    return 0;
+}
